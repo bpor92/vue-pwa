583b0ce75c7d80c22ef129e7b539a0776254300a
Bartek <bartoszporebski199@gmail.com>
2023-07-09 12:10:23 +0200

    init

---
+++ vite.config.ts
@@ -0,0 +1,50 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      devOptions: {
+        enabled: true
+      },
+      // display_override: ['tabbed'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'PWA learn',
+        short_name: 'PWA learn',
+        description: 'Opis apki',
+        theme_color: '#ffddd',
+        start_url: '/',
+        icons: [
+          {
+            src: '/svg-seeklogo.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: '/svg-seeklogo.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          },
+          {
+            src: '/svg-seeklogo.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    }
+    )
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
